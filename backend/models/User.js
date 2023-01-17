849d6d1dafabe5e4e0b9a2f15e15b850bb0d7d4c
Aurelie Gerber <gerber.prof@gmail.com>
2023-01-17 10:00:16 +0100

    refactor: change the comments

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");   // importer mongoose
 const uniqueValidator = require("mongoose-unique-validator");   // pour gérer plus facilement les erreurs générées par défaut par MongoDB
 
-const userSchema = mongoose.Schema({    // schéma mongoose de données pour chaque utilisateur (id automatiquement donné par la BDD)
+const userSchema = mongoose.Schema({    // schéma mongoose de données pour chaque utilisateur (id automatiquement généré par la BDD)
     email: {type: String, required: true, unique: true},    // unique pour empêcher plusieurs user avec la même adresse mail
     passwword: {type: String, required: true}
 });
 
 userSchema.plugin(uniqueValidator); // appliquer le validator au schéma avant d'en faire un modèle
 
-module.exports = mongoose.model("User", userSchema);    // exporter le schéma en tant que modèle mongoose appelé user, le rendant disponible pour notre app express
+module.exports = mongoose.model("User", userSchema);    // exporter le schéma en tant que modèle mongoose appelé User, le rendant disponible pour notre app express
