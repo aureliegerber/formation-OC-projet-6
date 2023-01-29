fd458a862ec8d694fee271370f383caf6805c735
Aurelie Gerber <gerber.prof@gmail.com>
2023-01-29 18:50:23 +0100

    refactor: add comments

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router();        // pour créer un routeur
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 const sauceCtrl = require("../controllers/sauce");
@@ -8,7 +8,7 @@ router.post("/", auth, multer, sauceCtrl.createSauce);  // auth à mettre avant 
 
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);    // en rajoutant multer le format de la requête change, il faut donc modifier le createSauce dans controllers/sauce
 
-router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);     // : dit à express que cette partie de la route ":id" est dynamique et on y a accès dans req.params.id car c'est un paramètre de route dynamique
 
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 
