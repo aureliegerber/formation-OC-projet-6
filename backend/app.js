337a5e44a55414046be2c31555f30574a24f57d8
Aurelie Gerber <gerber.prof@gmail.com>
2023-01-29 18:39:28 +0100

    feat: set up the environment variables

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,18 @@ const mongoose = require("mongoose"); // installer le package Mongoose (facilite
 const userRoutes = require("./routes/user");    // importer le routeur pour les utilisateurs
 const sauceRoutes = require("./routes/sauce");    // importer le routeur pour les sauces
 const path = require("path");
+require('dotenv').config();
 
 const app = express();  // créer une application express
 
 
-mongoose.connect("mongodb+srv://aureliegerber-OC:kZnXDAwUrnxnGHL1@cluster0.5tp1plk.mongodb.net/?retryWrites=true&w=majority", // connexion de l'API à la BDD
+mongoose.connect(process.env.MONGO_URL, // connexion de l'API à la BDD // chaine de connexion srv : SSL activé par défaut
 { useNewUrlParser: true,
   useUnifiedTopology: true })
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  .then(() => console.log("Successful connection to mongodb !"))
+  .catch(() => console.log("Connection to mongodb failed !"));
 
-  app.use((req, res, next) => { // middleware qui reçoit la requête et la réponse et qui les gère, sera appliqué à toutes les routes (d'où le req)    
+  app.use((req, res, next) => { // middleware qui reçoit la requête et la réponse et qui les gère, sera appliqué à toutes les routes   
     res.setHeader("Access-Control-Allow-Origin", "*");  // permettre d'accéder à notre API depuis n'importe quelle origine
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");  // donner l'autorisation d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");  // donner l'autorisation d'envoyer des requêtes avec les méthodes mentionnées
@@ -27,6 +28,6 @@ app.use("/api/auth", userRoutes);
 
 app.use("/api/sauces", sauceRoutes);
 
-app.use("/images", express.static(path.join(__dirname, "images")));   // pour gérer la requête vers le répertoire images
+app.use("/images", express.static(path.join(__dirname, "images")));   // pour gérer la requête vers le répertoire images, on récupère le répertoire dans lequel s'exécute notre server et on y concatène le dossier images
 
 module.exports = app; //  exporter l'application pour y accéder depuis les autres fichiers de notre projet notamment le server node
